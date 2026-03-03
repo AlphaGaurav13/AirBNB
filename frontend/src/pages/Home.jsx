b490d34563c563786e4e55ffc93194c9875381b1
Gaurav132005 <gauravkkr345@gmail.com>
2026-03-04 01:17:17 +0530

    Build Auth frontEnd

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='text-[30px]'>
+            home Page
+        </div>
+    )
+}
+
+export default Home
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { FaEye } from "react-icons/fa6";
+import { IoMdEyeOff } from "react-icons/io";
+import {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+import { FaArrowLeftLong } from "react-icons/fa6";
+function SignUp() {
+    let navigate = useNavigate();
+    let [Show, setShow] = useState(false);
+    return (
+        <div className='w-[100vw] h-[100vh] flex items-center justify-center relative'>
+
+            <div className="w-[50px] h-[50px] flex justify-center items-center absolute top-[10%] left-[20px] rounded-[50%] bg-[red] text-[white]" onClick={() => navigate("/")}><FaArrowLeftLong className="w-[25px] h-[25px] text-white"/></div>
+
+            <form action="" className="max-w-[900px] w-[90%] h-[600px] flex items-center justify-center flex-col md:items-start">
+                <h1 className="text-[30px] text-[black]">Welcome to AirBnb</h1>
+                <div className="w-[90%] flex items-start justify-start flex-col gap-[10px] mt-[30px]">
+                    <label htmlFor="name"  className="text-[20px]">UserName</label>
+                    <input type="text" id="name" className="p-[10px] w-[90%] h-[40px] border-[2px] border-[#555656] rounded-lg"/>
+                </div>
+
+                <div className="w-[90%] flex items-start justify-start flex-col gap-[10px]">
+                    <label htmlFor="email" className="text-[20px]">Email</label>
+                    <input type="text" id="email" className="p-[10px] w-[90%] h-[40px] border-[2px] border-[#555656] rounded-lg"/>
+                </div>
+
+                <div className="w-[90%] flex items-start justify-start flex-col gap-[10px] relative">
+                    <label htmlFor="password" className="text-[20px]">Password</label>
+                    <input type={Show?"password":"text"} id="password" className="p-[10px] w-[90%] h-[40px] border-[2px] border-[#555656] rounded-lg "/>
+
+                    {!Show && (
+                        <FaEye className="w-[22px] h-[22px] absolute right-[12%] bottom-[10px]" 
+                        onClick={() => setShow(prev => !prev)}
+                        />
+                    )}
+
+                    {Show && (
+                        <IoMdEyeOff  className="w-[22px] h-[22px] absolute right-[12%] bottom-[10px]"
+                        onClick={() => setShow(prev => !prev)}
+                        />
+                    )}
+
+                </div>
+
+
+                <button className="px-[50px] py-[10px] bg-[red] text-[white] rounded-lg mt-[20px] md:px-[100px]">SignUp</button>
+                <p className='text-[15px]'>Already have an Account? <span className='text-[15px] text-[red] cursor-pointer' onClick={() => navigate("/login")}>Login</span></p>
+            </form>
+        </div>
+    )
+}
+
+export default SignUp
